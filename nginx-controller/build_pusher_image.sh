dd5d6c744ed4ad76c254fa374c0e71b6cd4202f6
danigosa <dani.gonzalez@infantium.com>
2016-10-15 11:50:57 -0400

    improve SSL config

--- nginx-controller/build_pusher_image.sh
+++ nginx-controller/build_pusher_image.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 make clean
-make PREFIX=eu.gcr.io/infantium-platform-20/nginx-pusher-ingress TAG=0.1 PUSH_TO_GCR=1
+make PREFIX=eu.gcr.io/infantium-platform-20/nginx-ingress TAG=latest PUSH_TO_GCR=1
