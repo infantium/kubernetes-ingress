5f7ad6001c90b9bcb254b7e99095015292ea691d
danigosa <dani.gonzalez@infantium.com>
2017-08-13 16:44:07 -0400

    upgrade nginx ingress

--- nginx-controller/build_pusher_image.sh
+++ nginx-controller/build_pusher_image.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 gcloud docker -- pull gcr.io/robbie-ai/gcloud-server-alpine36/latest:latest
 make clean
-make PREFIX=gcr.io/robbie-ai/nginx-ingress TAG=alpine36.v2 PUSH_TO_GCR=1
+make PREFIX=gcr.io/robbie-ai/nginx-ingress TAG=alpine36.v3.1 PUSH_TO_GCR=1
---
+++ nginx-controller/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+# Use your privileges...
+sysctl -p
+
+# Exec ingress
+/nginx-ingress
